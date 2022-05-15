1ec510470671ae7aefaa7055b48476c0e84df140
adammathewvaughn <adammathewvaughn@gmail.com>
2022-05-14 23:35:04 -0500

    not working but not crashing

--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -1,12 +1,15 @@
 #Lab 2
 
+
+
+
 def file_copier(file):
     """
     This function reads a file and writes the information into a new file.
     """
 
-    file_name = input(randomlist.txt)
-    file_name_new = input(randomlist_new.txt)
+    file_name = input(names.txt)
+    file_name_new = input(new_names.txt)
     with open(file_name, "r") as file_to_copy:
         with open(file_name_new, "w") as new_file:
             for line in file_to_copy:
