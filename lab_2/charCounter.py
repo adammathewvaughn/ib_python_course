5e082a291045fde2e6ab2370657d7750ba786abb
adammathewvaughn <adammathewvaughn@gmail.com>
2022-05-20 23:52:54 -0500

    still not looping through correctly

--- lab_2/charCounter.py
+++ lab_2/charCounter.py
@@ -1,5 +1,3 @@
-#Lab 2
-
 
 
 
@@ -8,11 +6,11 @@ def char_counter(file):
     This function loops through the given file and counts each character, returning the count as an integer.
 
     """
-
-    file = open("names.txt", 'r')
-    count = 0
-    for count in file:
+    file = open(file, 'r')
+    for i in file:
+        count = 0
         count = count + 1
+    file.close()
     return int(count)
 
 print (char_counter("names.txt"))
